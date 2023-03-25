e1b2cfcc64dc191d262ad3382d43885b1c7ad42e
Mason Francis <a02244655@usu.edu>
2023-03-25 01:29:33 -0600

    Updated to remember chatperson in db

--- main.py
+++ main.py
@@ -1,64 +1,58 @@
 import openai
 import json
-#import mysql.connector
+import mysql.connector
 
 if __name__ == '__main__':
     openAiFile = open('openaiauth.json')
     openai.api_key =  json.load(openAiFile)["key"]
     openAiFile.close()
 
-    # dbInfoFile = open('db.json')
-    # dbInfo = json.load(dbInfoFile)
-    # dbInfoFile.close()
+    dbInfoFile = open('db.json')
+    dbInfo = json.load(dbInfoFile)
+    dbInfoFile.close()
 
-    # dbConn = mysql.connector.connect(
-    #     host=dbInfo['address'],
-    #     user=dbInfo['user'],
-    #     password=dbInfo['password']
-    # )
-    # dbCursor = dbConn.cursor()
+    dbConn = mysql.connector.connect(
+        host=dbInfo['address'],
+        user=dbInfo['user'],
+        password=dbInfo['password']
+    )
+    dbCursor = dbConn.cursor()
 
-    # phoneNumber = "+13135551234"
-
-    # getQuery = f"SELECT * FROM rejection.dynamic_chatbot where phoneNumber = '{phoneNumber}' ORDER BY timeGenerated DESC"
-
-    botPerson = "Joe Biden"
+    
 
     while True:
 
+        phoneNumber = "+13135551234"
+
+        getQuery = f"SELECT * FROM rejection.chatbot_settings where phoneNumber = '{phoneNumber}'"
+        dbCursor.execute(getQuery)
+
+        getResult = dbCursor.fetchone()
+        botPerson = "Donald Trump"
+        if getResult == None:
+            createQuery = "INSERT INTO rejection.chatbot_settings (phoneNumber, chatPerson) VALUES (%s, %s)"
+            dbCursor.execute(createQuery, (phoneNumber, botPerson))
+            dbConn.commit()
+        else:
+            botPerson = getResult[2]
         chatMessages = [{"role": "system", "content": f"You are {botPerson}. You should respond to all messages in the speaking style of {botPerson}"}]
 
-        # dbCursor.execute(getQuery)
-
-        # getResult = dbCursor.fetchone()
-
-
-        # chatMessages = []
-        # if getResult == None:
-        #     chatMessages = [{"role": "system", "content": f"You are {botPerson}. You should respond to all messages in the speaking style of {botPerson}"}]
-        # else:
-        #     strList = getResult[2].split('&')
-        #     jsonList = []
-        #     for x in strList:
-        #         jsonList.append(json.loads(x))
-        #     chatMessages = jsonList
-    
-
         userInput = input("Enter a message to send: ")
-        chatMessages.append({"role": "user", "content": userInput})
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=chatMessages
-        )
-        chatMessages.append({"role": response['choices'][0]['message']['role'], "content": response['choices'][0]['message']['content']})
 
-        # saveQuery = "INSERT INTO rejection.dynamic_chatbot (conversation, phoneNumber) VALUES (%s, %s)"
-        # valueStrList = []
-        # for x in chatMessages:
-        #     valueStrList.append(str(x))
-        # valueStr = '&'.join(valueStrList)
-        # saveValues = (valueStr, phoneNumber)
-        # dbCursor.execute(saveQuery, saveValues)
-        # dbConn.commit()
+        # Check for command
+        if userInput[0:1] == "#":
+            if userInput[1:].split()[0] == "changeperson":
+                botPerson = ' '.join(userInput.split()[1:])
+                updateQuery = f"UPDATE rejection.chatbot_settings SET chatPerson = '{botPerson}' where phoneNumber = {phoneNumber}"
+                dbCursor.execute(updateQuery)
+                dbConn.commit()
 
-        print("Response: " + response['choices'][0]['message']['content'])
+        else:
+            chatMessages.append({"role": "user", "content": userInput})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=chatMessages
+            )
+            chatMessages.append({"role": response['choices'][0]['message']['role'], "content": response['choices'][0]['message']['content']})
+
+            print("Response: " + response['choices'][0]['message']['content'])
