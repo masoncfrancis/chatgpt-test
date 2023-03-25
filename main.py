fc5e66a2fda982cf018f7d1d96e7505093fd543c
Mason Francis <a02244655@usu.edu>
2023-03-25 00:44:14 -0600

    Commented out all db stuff bc it doesn't work right
    and its pissing me off

--- main.py
+++ main.py
@@ -7,32 +7,41 @@ if __name__ == '__main__':
     openai.api_key =  json.load(openAiFile)["key"]
     openAiFile.close()
 
-    dbInfoFile = open('db.json')
-    dbInfo = json.load(dbInfoFile)
-    dbInfoFile.close()
+    # dbInfoFile = open('db.json')
+    # dbInfo = json.load(dbInfoFile)
+    # dbInfoFile.close()
 
-    dbConn = mysql.connector.connect(
-        host=dbInfo['address'],
-        user=dbInfo['user'],
-        password=dbInfo['password']
-    )
-    dbCursor = dbConn.cursor()
+    # dbConn = mysql.connector.connect(
+    #     host=dbInfo['address'],
+    #     user=dbInfo['user'],
+    #     password=dbInfo['password']
+    # )
+    # dbCursor = dbConn.cursor()
 
-    phoneNumber = "+13135551234"
+    # phoneNumber = "+13135551234"
 
-    getQuery = f"SELECT * FROM rejection.dynamic_chatbot where phoneNumber = '{phoneNumber}' ORDER BY timeGenerated DESC"
+    # getQuery = f"SELECT * FROM rejection.dynamic_chatbot where phoneNumber = '{phoneNumber}' ORDER BY timeGenerated DESC"
+
+    botPerson = "Joe Biden"
 
     while True:
 
-        dbCursor.execute(getQuery)
+        chatMessages = [{"role": "system", "content": f"You are {botPerson}. You should respond to all messages in the speaking style of {botPerson}"}]
 
-        getResult = dbCursor.fetchone()
+        # dbCursor.execute(getQuery)
 
-        chatMessages = []
-        if getResult == None:
-            chatMessages = [{"role": "system", "content": "You are Donald Trump. You should respond to all messages in the speaking style of Donald Trump"}]
-        else:
-            chatMessages = [json.loads(getResult)]
+        # getResult = dbCursor.fetchone()
+
+
+        # chatMessages = []
+        # if getResult == None:
+        #     chatMessages = [{"role": "system", "content": f"You are {botPerson}. You should respond to all messages in the speaking style of {botPerson}"}]
+        # else:
+        #     strList = getResult[2].split('&')
+        #     jsonList = []
+        #     for x in strList:
+        #         jsonList.append(json.loads(x))
+        #     chatMessages = jsonList
     
 
         userInput = input("Enter a message to send: ")
@@ -43,9 +52,13 @@ if __name__ == '__main__':
         )
         chatMessages.append({"role": response['choices'][0]['message']['role'], "content": response['choices'][0]['message']['content']})
 
-        saveQuery = "INSERT INTO rejection.dynamic_chatbot (conversation, phoneNumber) VALUES (%s, %s)"
-        saveValues = (str(chatMessages), phoneNumber)
-        dbCursor.execute(saveQuery, saveValues)
-        dbConn.commit()
+        # saveQuery = "INSERT INTO rejection.dynamic_chatbot (conversation, phoneNumber) VALUES (%s, %s)"
+        # valueStrList = []
+        # for x in chatMessages:
+        #     valueStrList.append(str(x))
+        # valueStr = '&'.join(valueStrList)
+        # saveValues = (valueStr, phoneNumber)
+        # dbCursor.execute(saveQuery, saveValues)
+        # dbConn.commit()
 
         print("Response: " + response['choices'][0]['message']['content'])
